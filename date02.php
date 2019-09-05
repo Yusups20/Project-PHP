27ff71453b5bd2c613cf4028d6a938c1a0a9d81a
Yusups20 <yups2092003.com>
2019-09-05 11:18:44 +0700

    date

---
+++ date02.php
@@ -0,0 +1,7 @@
+<?php
+// Sekarang tanggal 13 april 2006 09:43:49
+$arrDAy = array("MInggu","Senin","Selasa","Rabu","KAmis","JUm'at","Sabtu");
+$day = date ("w"); //0 - 6 of day
+
+echo "HAri ini hari : <b>".$arrDAy[$day]."</b>"
+?>
